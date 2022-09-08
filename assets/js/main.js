681bae92cabc713cb238f4fadda2cf4d49fd240e
Joshua <joshuadami17@gmail.com>
2022-09-08 18:16:12 +0000

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: EstateAgency - v4.8.0
-* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
@@ -83,22 +77,6 @@
   }
 
   /**
-   * Search window open/close
-   */
-  let body = select('body');
-  on('click', '.navbar-toggle-box', function(e) {
-    e.preventDefault()
-    body.classList.add('box-collapse-open')
-    body.classList.remove('box-collapse-closed')
-  })
-
-  on('click', '.close-box-collapse', function(e) {
-    e.preventDefault()
-    body.classList.remove('box-collapse-open')
-    body.classList.add('box-collapse-closed')
-  })
-
-  /**
    * Intro Carousel
    */
   new Swiper('.intro-carousel', {
@@ -142,53 +120,6 @@
         slidesPerView: 3,
         spaceBetween: 20
       }
-    }
-  });
-
-  /**
-   * News carousel
-   */
-  new Swiper('#news-carousel', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.news-carousel-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
-  /**
-   * Testimonial carousel
-   */
-  new Swiper('#testimonial-carousel', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.testimonial-carousel-pagination',
-      type: 'bullets',
-      clickable: true
     }
   });
 
--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,8 +1,3 @@
-/**
-* PHP Email Form Validation - v3.4
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
 (function () {
   "use strict";
 
@@ -16,7 +11,7 @@
 
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
+
       if( ! action ) {
         displayError(thisForm, 'The form action property is not set!')
         return;
