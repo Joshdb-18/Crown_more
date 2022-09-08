39916949225d85ad6f2bf76ac082d26220a8fb54
Joshua <joshuadami17@gmail.com>
2022-09-08 10:49:11 +0000

    p

--- forms/contact.php
+++ forms/contact.php
@@ -9,16 +9,16 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'albertjoshua122@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+ // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+   // include( $php_email_form );
+  //} else {
+   // die( 'Unable to load the "PHP Email Form" Library!');
+  //}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
+ // $contact = new PHP_Email_Form;
+ // $contact->ajax = true;
+
+  //$contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
@@ -36,6 +36,6 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
+  mail($receiving_email_address, $contact->subject, $contact->add_message);
   echo $contact->send();
 ?>
