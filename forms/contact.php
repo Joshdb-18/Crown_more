7975c86dd36083f43799a4481d40d3a1f79d4484
Cidscaptal <jesse.jason2002@gmail.com>
2022-09-08 23:55:45 +0300

    Added mymail.php file to allow mail sending from localhost

--- forms/contact.php
+++ forms/contact.php
@@ -3,7 +3,7 @@ if ($_POST) {
 	$visitor_name = $_POST['name'];
 	$visitor_email = $_POST['email'];
 	$email_subject = $_POST['subject'];
-	$email_body = ""
+	$email_body = "";
 	if(isset($_POST['message'])) {
         $visitor_message = htmlspecialchars($_POST['message']);
         $email_body .= "<div>
@@ -16,7 +16,7 @@ if ($_POST) {
 	.'Content-type: text/html; charset=utf-8' . "\r\n"
 	.'From: ' . $visitor_email . "\r\n";
 	$recipient = "albertjoshua122@gmail.com";
-	mail($recipient, $email_body, $headers)
+	mail($recipient, $email_body, $headers);
 } else {
 	echo '<p>Something went wrong</p>';
 }
