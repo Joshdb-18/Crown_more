056033e61801e6316e7cf53aaf9a77d5df2e2384
Joshua <joshuadami17@gmail.com>
2022-09-11 17:50:04 +0000

    fixed mail folder

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,36 @@
 <?php
-if ($_POST) {
-	$visitor_name = $_POST['name'];
-	$visitor_email = $_POST['email'];
-	$email_subject = $_POST['subject'];
-	$email_body = "";
-	if(isset($_POST['message'])) {
-        $visitor_message = htmlspecialchars($_POST['message']);
-        $email_body .= "<div>
-                           <label><b>Visitor Message:</b></label>
-                           <div>".$visitor_message."</div>
-                        </div>";
+$errors = [];
+$errorMessage = '';
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
     }
-	$email_body = "</div>";
-	$headers = 'MIME-Version: 1.0' . "\r\n"
-	.'Content-type: text/html; charset=utf-8' . "\r\n"
-	.'From: ' . $visitor_email . "\r\n";
-	$recipient = "albertjoshua122@gmail.com";
-	mail($recipient, $email_body, $headers);
-} else {
-	echo '<p>Something went wrong</p>';
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+    if (empty($errors)) {
+        $toEmail = 'albertjoshua122@gmail.com';
+        $emailSubject = $subject;
+        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+        $body = join(PHP_EOL, $bodyParagraphs);
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: index.html');
+        } else {
+            $errorMessage = 'Oops, something went wrong. Please try again later';
+        }
+    } else {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    }
 }
-
 ?>
