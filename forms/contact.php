ada145620eac07564977f22eb3927d72aeef9d2d
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2022-12-26 11:07:16 +0000

    update mail form

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,78 @@
 <?php
-$errors = [];
-$errorMessage = '';
-if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
-    if (empty($errors)) {
-        $toEmail = 'odamilola958@gmail.com';
-        $emailSubject = $subject;
-        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
-        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
-        $body = join(PHP_EOL, $bodyParagraphs);
-        if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: index.html');
-        } else {
-            $errorMessage = 'Oops, something went wrong. Please try again later';
-        }
-    } else {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-    }
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$crownmore_email = "odamilola958@gmail.com";
+
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$feedback_page = "feedback.html";
+$error_page = "error.html";
+$thankyou_page = "success.html";
+
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$email_address = $_REQUEST['email'] ;
+$subject = $_REQUEST['subject'] ;
+$name = $_REQUEST['name'] ;
+$message = $_REQUEST['message'] ;
+$msg = 
+"Name: " . $name . "\r\n" . 
+"Email: " . $email . "\r\n" . 
+"Subject: " . $subject . "\r\n" .
+"Message: " . $message ;
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// If the user tries to access this script directly, redirect them to the feedback form,
+if (!isset($_REQUEST['email'])) {
+header( "Location: $feedback_page" );
+}
+
+// If the form fields are empty, redirect to the error page.
+elseif (empty($first_name) || empty($email_address)) {
+header( "Location: $error_page" );
+}
+
+/* 
+If email injection is detected, redirect to the error page.
+If you add a form field, you should add it here.
+*/
+elseif ( isInjected($email_address) || isInjected($name)  || isInjected($subject) || isInjected($message) ) {
+header( "Location: $error_page" );
+}
+
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+
+	mail( "$crownmore_email", "Feedback Form Results", $msg );
+
+	header( "Location: $thankyou_page" );
 }
 ?>
